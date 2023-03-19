fc27f0fa32fc697fd84b72245166602495f772a6
RaySummer <ray__summer@hotmail.com>
2023-03-19 19:53:51 +0800

    优化

--- portal/src/main/java/com/eco/commerce/portal/module/member/controller/MemberController.java
+++ portal/src/main/java/com/eco/commerce/portal/module/member/controller/MemberController.java
@@ -1,16 +1,13 @@
 package com.eco.commerce.portal.module.member.controller;
 
 import com.eco.commerce.core.utils.Response;
+import com.eco.commerce.core.utils.WebThreadLocal;
 import com.eco.commerce.portal.module.member.dto.ro.MemberRO;
 import com.eco.commerce.portal.module.member.dto.ro.MemberRegisterRO;
 import com.eco.commerce.portal.module.member.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Ray
@@ -37,5 +34,10 @@ public class MemberController {
     @PostMapping("/modify")
     public Response modify(@RequestBody MemberRO ro) {
         return Response.of(memberService.modify(ro));
+    }
+
+    @GetMapping("/member-info")
+    public Response getMemberInfo(){
+        return Response.of(WebThreadLocal.getMember());
     }
 }
